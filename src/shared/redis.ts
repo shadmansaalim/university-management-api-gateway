6bbbaa9abf41bf99cfbc8e87bb6df5255f00f105
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-26 14:31:55 +1100

    Redis connected to project and connection code implemented in redis.ts in shared folder

---
+++ src/shared/redis.ts
@@ -0,0 +1,22 @@
+// Imports
+import { createClient } from 'redis';
+import logger from './logger';
+
+let redisClient = createClient({
+  url: 'redis://localhost:6379'
+});
+
+// Redis error logging
+redisClient.on('error', (err) => logger.error('Redis Error', err));
+
+// Redis connected successfully
+redisClient.on('connect', () => logger.info('Redis Connected'));
+
+// Function to connect redis client
+const connect = async (): Promise<void> => {
+  await redisClient.connect();
+};
+
+export const RedisClient = {
+  connect
+};
