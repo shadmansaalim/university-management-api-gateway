bfb24ab70662b4e0f232ddf78ef95d7beb56af90
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-26 19:22:56 +1100

    Auth Guard added

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,5 +1,7 @@
 // Imports
 import express from 'express';
+import { ENUM_USER_ROLES } from '../../../enums/user';
+import authGuard from '../../middlewares/authGuard';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
@@ -11,14 +13,20 @@ router.get('/:id', AcademicSemesterController.getByIdFromDb);
 router.get('/', AcademicSemesterController.getAllFromDb);
 router.post(
   '/',
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
   validateRequest(AcademicSemesterValidation.create),
   AcademicSemesterController.insertIntoDb
 );
 router.patch(
   '/:id',
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
   validateRequest(AcademicSemesterValidation.update),
   AcademicSemesterController.updateByIdIntoDb
 );
-router.delete('/:id', AcademicSemesterController.deleteByIdIntoDb);
+router.delete(
+  '/:id',
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
+  AcademicSemesterController.deleteByIdIntoDb
+);
 
 export const AcademicSemesterRoutes = router;
