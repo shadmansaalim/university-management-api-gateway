c7c2f67ff6a3a44d00ebfd625c6d3cc2395d8f6d
shadmansaalim <shadmansaalim999@gmail.com>
2023-10-29 23:05:29 +1100

    Faculty and Admin create routes implemented

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { ENUM_USER_ROLES } from '../../../enums/user';
-import authGuard from '../../middlewares/authGuard';
 import { UserController } from './user.controller';
-import { FileUploadHelpers } from '../../../helpers/fileUploadHelpers';
 import { UserValidation } from './user.validation';
+import authGuard from '../../middlewares/authGuard';
+import { ENUM_USER_ROLES } from '../../../enums/user';
+import { FileUploadHelpers } from '../../../helpers/fileUploadHelpers';
 
 const router = express.Router();
 
@@ -14,6 +14,26 @@ router.post(
   (req: Request, res: Response, next: NextFunction) => {
     req.body = UserValidation.createStudent.parse(JSON.parse(req.body.data));
     return UserController.createStudent(req, res, next);
+  }
+);
+
+router.post(
+  '/create-faculty',
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
+  FileUploadHelpers.upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = UserValidation.createFaculty.parse(JSON.parse(req.body.data));
+    return UserController.createFaculty(req, res, next);
+  }
+);
+
+router.post(
+  '/create-admin',
+  authGuard(ENUM_USER_ROLES.ADMIN, ENUM_USER_ROLES.SUPER_ADMIN),
+  FileUploadHelpers.upload.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = UserValidation.createAdmin.parse(JSON.parse(req.body.data));
+    return UserController.createAdmin(req, res, next);
   }
 );
 
